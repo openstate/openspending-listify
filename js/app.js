1b15a4411c30681112059ad7bec5d8433099e745
Breyten Ernsting <bje@dds.nl>
2015-11-11 18:08:06 +0100

    Get a sample document to retrieve the labels from.

--- js/app.js
+++ js/app.js
@@ -27,7 +27,36 @@ OpenspendingListify.init = function() {
     $('#choice-plan').text($('#form-plan input:checked').parent().text());
     $('#choice-direction').text($('#form-direction input:checked').parent().text());
     $('#choice-year').text($('#form-year input:checked').parent().text());
-  })
+  });
+
+  $('#btn-submit').on('click', function (e) {
+    OpenspendingListify.submit();
+    e.preventDefault();
+    return false;
+  });
+};
+
+OpenspendingListify.get_sample_document = function(kind, year, period, plan) {
+  // TODO: make it return one document, since that is all we need anyway ...
+  var docs_url = 'http://www.openspending.nl/api/v1/documents/?government__kind=' + kind + '&year=' + year + '&period=' + period + '&plan=' + plan + '&format=json'
+  console.log(docs_url);
+  $.get(docs_url, function (data) {
+    console.log('got data:');
+    console.dir(data);
+  });
+};
+
+OpenspendingListify.submit = function() {
+ // http://www.openspending.nl/api/v1/documents/?government__kind=county&year=2014&period=5&plan=spending&format=json
+  console.log('form submitted!');
+  var size = $('#form-size input:checked').val();
+  var order = $('#form-order input:checked').val();
+  var kind = $('#form-kind input:checked').val();
+  var plan = $('#form-plan input:checked').val();
+  var direction = $('#form-direction input:checked').val();
+  var year = $('#form-year input:checked').val();
+  var period = (plan == "budget") ? 0 : 5; // TODO: implement kwartalen
+  OpenspendingListify.get_sample_document(kind, year, period, plan);
 };
 
 $(document).ready(function() {
